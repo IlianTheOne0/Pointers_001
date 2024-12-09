81e6fcd5b6ea380cea84ef0720c93ecc49bf99e8
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-09 19:20:55 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,25 @@ using std::cin;
 
 int main()
 {
+	int *first_number = new int;
+	int *second_number = new int;
+
+	cout << "Enter the first number: ";
+	cin >> *first_number;
+	cout << "Enter the second number: ";
+	cin >> *second_number;
+
+	if (*first_number < *second_number)
+	{
+		cout << *second_number << " is bigger, than " << *first_number << endl;
+	}
+	else if (*first_number > *second_number)
+	{
+		cout << *first_number << " is bigger, than " << *second_number << endl;
+	}
+	else {
+		cout << *first_number << " equals " << *second_number << endl;
+	}
 
 	_getch();
 	return 0;
