7f6f3bd929a940befee8d0eb0e94382fa08cbd73
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-09 20:11:56 +0200

    Completed Task 4

--- main.cpp
+++ main.cpp
@@ -5,9 +5,50 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+double* first_number = new double;
+double* second_number = new double;
+
+int* selected = new int;
+double* result = new double;
+
+void _action()
+{
+	switch (*selected)
+	{
+	case 1: *result = *first_number + *second_number; break;
+	case 2: *result = *first_number - *second_number; break;
+	case 3: *result = *first_number * *second_number; break;
+	case 4: *result = *first_number / *second_number; break;
+	case 5: *result = (*first_number / 100) * *second_number; break;
+	}
+}
+
 int main()
 {
+	cout << "Enter the first number: ";
+	cin >> *first_number;
+	cout << "Enter the second number: ";
+	cin >> *second_number;
+
+	cout << endl << "Choose the option" << endl
+		<< "1. SUM" << endl
+		<< "2. MINUS" << endl
+		<< "3. MULTIPLICATION" << endl
+		<< "4. DIVISION" << endl
+		<< "5. PERCENTAGE" << endl
+		<< "Option: ";
+	cin >> *selected;
+
+	_action();
+
+	cout << endl << "Result: " << *result;
 
 	_getch();
+
+	delete first_number;
+	delete second_number;
+	delete selected;
+	delete result;
+
 	return 0;
 }
